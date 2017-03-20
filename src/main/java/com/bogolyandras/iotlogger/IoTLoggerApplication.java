9712587ed9aeb5dbea5aca2803f277e4e5c5be66
Andras Bogoly <ebogoly@gmail.com>
2017-03-20 21:58:02 +0100

    First working software

---
+++ src/main/java/com/bogolyandras/iotlogger/IoTLoggerApplication.java
@@ -0,0 +1,12 @@
+package com.bogolyandras.iotlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTLoggerApplication.class, args);
+	}
+}
